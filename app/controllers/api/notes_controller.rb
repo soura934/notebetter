ff614b7f397519191a1a60090dca0934dd8491fa
soura934 <soura934@gmail.com>
2021-01-26 15:54:39 -0500

    testing my git

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -1,13 +1,32 @@
-class NotesController < ApplicationController
-  def new
+class Api::Notes < ApplicationController
+
+  def index 
+    @notes = Note.all
+    render :index
+  end
+
+  def show
+    @note = Note.find(params[:id])
+    render :show
   end
 
   def create
+    @note = Note.new(valid_params)
+    @note.save
+    if @note
+      render :show
+    else
+      render .full_messages
+
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def valid_params
+    params.require(:note).permit(:title, :body)
   end
 end
