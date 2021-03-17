a6a63dcb75cf6d3b561dc116baa9bf8eb371c8ac
soura934 <soura934@gmail.com>
2021-03-16 23:53:06 -0400

    style navbar

--- frontend/components/greeting/sidebar.jsx
+++ frontend/components/greeting/sidebar.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Sidebar = (props) => {
         return (
-        <div className="wrapper">
+        <div className="sidebar">
             
-            <div className="sidebar">
+            <div className="sidebar-pp">
             {props.currentUser.email} 
             <div id="logout">
             <button  onClick={props.logout}>Logout</button>
