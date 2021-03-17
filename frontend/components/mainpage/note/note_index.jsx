a71fe84c0d8fb17ea9e4e008e996e0a93d535415
soura934 <soura934@gmail.com>
2021-03-16 23:56:10 -0400

    hide notes

--- frontend/components/mainpage/note/note_index.jsx
+++ frontend/components/mainpage/note/note_index.jsx
@@ -12,20 +12,20 @@ class NoteIndex extends React.Component {
 
 
     render() { 
-        if (this.props.notes === {}){
-            return <p>No notes yet!</p>;
-        } 
-        const noteLi = this.props.notes.map(note => {
-            return <li key={note.id}>
-                <h2>{note.title}</h2>
-                <p>{note.body}</p>
-            </li>
-        });
-        return ( 
-            <div>
-                {noteLi}
-            </div>
-         );
+    //     if (this.props.notes === {}){
+    //         return <p>No notes yet!</p>;
+    //     } 
+    //     const noteLi = this.props.notes.map(note => {
+    //         return <li key={note.id}>
+    //             <h2>{note.title}</h2>
+    //             <p>{note.body}</p>
+    //         </li>
+    //     });
+    //     return ( 
+    //         <div>
+    //             {noteLi}
+    //         </div>
+    //      );
     }
 }
  
