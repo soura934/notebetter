e5062f38e8de3c96150f2db1b7689a0fa43e031c
soura934 <soura934@gmail.com>
2021-03-21 20:54:37 -0400

    done note list header

--- frontend/components/mainpage/note/note_index.jsx
+++ frontend/components/mainpage/note/note_index.jsx
@@ -12,8 +12,17 @@ class NoteIndex extends React.Component {
 
     render() { 
         return ( 
-        // <div className="note">hello</div>
-            <NoteList notes={this.props.notes}/>
+        <div className="list-of-notes">
+            <div className="notes-header">
+                <div className="notes-header-title">
+                    <h1>All Notes</h1>
+                </div>
+                <div className="notes-sub-header">
+                    <p className="notes-count">8 notes</p>
+                </div>
+            </div>
+        </div>
+            // <NoteList notes={this.props.notes}/>
 
          );
     }
