5de495a5246a7f71c1c51d24278a7e76250e0069
soura934 <soura934@gmail.com>
2021-04-12 23:52:47 -0400

    create columns

--- db/migrate/20210413034414_create_notebook.rb
+++ db/migrate/20210413034414_create_notebook.rb
@@ -1,6 +1,12 @@
 class CreateNotebook < ActiveRecord::Migration[5.2]
   def change
     create_table :notebooks do |t|
+      t.string :title, null: false
+      t.integer :user_id, null: false 
+
+      t.timestamps
     end
+
+    
   end
 end
