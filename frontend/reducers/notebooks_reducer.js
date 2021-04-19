406b4de40b4630a0509f782b3072050fa32c6abd
soura934 <soura934@gmail.com>
2021-04-18 22:58:45 -0400

    set up notebook reducer

---
+++ frontend/reducers/notebooks_reducer.js
@@ -0,0 +1,9 @@
+import { 
+    RECEIVE_NOTEBOOK,
+    RECEIVE_NOTEBOOKS,
+    DESTROY_NOTEBOOK
+ } from "../actions/notebook_actions";
+
+ const notebooksReducer = (oldState = {}, action) => {
+    
+ }
