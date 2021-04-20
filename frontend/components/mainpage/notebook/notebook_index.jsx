73bb59d83c6cffe8b05f78593bb4e774c2707b59
soura934 <soura934@gmail.com>
2021-04-19 23:35:50 -0400

    notebook header tree

--- frontend/components/mainpage/notebook/notebook_index.jsx
+++ frontend/components/mainpage/notebook/notebook_index.jsx
@@ -3,22 +3,46 @@ import React from "react";
 class NotebookIndex extends React.Component {
     constructor(props) {
         super(props);
-
+        this.count = this.count.bind(this);
     }
     componentDidMount(){
         this.props.fetchNotebooks();
+    }
+    count(){
+        if (this.props.notebooks.length > 1){
+            return <h2>{this.props.notebooks.length} notebook</h2>
+        } else {
+            return <h2>{this.props.notebooks.length} notebooks</h2>
+        }
+
     }
     render() { 
         if (this.props.notebooks === {}){
             return <p>No notebooks yet!</p>;
         }
+
         const notebookLi = this.props.notebooks.map(notebook => {
-            return <li key={notebook.id}>
-                {notebook.title}
-            </li>
+            return  <li key={notebook.id}>
+                        {notebook.title}
+                    </li>
+
         })
+        
         return ( <>
-                {notebookLi}
+                <div className="notebook_index">
+                    <div className="notebook-header">
+                        <div className="notebook-header-top">
+                            <h1>Notebooks</h1>
+                        </div>
+                        <div className="notebook-header-count">
+                            {this.count()}
+                        </div>
+                        <div className="new-notebook">
+                            <h1>New Notebook</h1>
+                        </div>
+                    </div>
+                    {notebookLi}
+                </div>
             </>
          );
     }
