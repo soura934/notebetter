a50acea98ec52e9143192a1f71638987bb0e387d
soura934 <soura934@gmail.com>
2021-04-20 23:52:19 -0400

    create notebook form boilplate

---
+++ frontend/components/mainpage/notebook/create_notebook_form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class NotebookForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( null );
+    }
+}
+ 
+export default NotebookForm;
