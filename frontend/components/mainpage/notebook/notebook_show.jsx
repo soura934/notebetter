69224d76b8bf4fba6c2808804a84a3799f1af01c
soura934 <soura934@gmail.com>
2021-04-24 23:59:16 -0400

    take out unnecessary lines

--- frontend/components/mainpage/notebook/notebook_show.jsx
+++ frontend/components/mainpage/notebook/notebook_show.jsx
@@ -4,12 +4,11 @@ import NoteIndex from "../note/note_index_container"
 class NotebookShow extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
     }
     render() { 
-        // if (!this.props.notes) {
-        //     return <div>no notebooks yet</div>
-        // } 
+        if (!this.props.notes) {
+            return <div>no notebooks yet</div>
+        } 
         return ( 
             <NoteIndex notes={this.props.notes} />
          );
--- frontend/components/mainpage/notebook/notebook_show_container.js
+++ frontend/components/mainpage/notebook/notebook_show_container.js
@@ -6,7 +6,7 @@ import NotebookShow from './notebook_show';
 const mstp = (state, ownProps) => {
     debugger
     return {
-        // notes: state.entities.notebooks[ownProps.match.params.notebookId].notes
+        notes: state.entities.notebooks[ownProps.match.params.notebookId].notes
     }
 }
 
